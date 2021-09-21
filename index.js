1c1705c72d4b18aff91a2fcc7a85eddc9ac85897
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-09-20 17:38:36 -0700

    commit tree c2a9250208da1ff2d2a163198248eeee665a7a24

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(num){
+  if (num <= 400){
+    return 'This one is on me!'
+  } else if (num > 2000 && num < 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (num > 2500){
+    return 'No can do.'
+  }
+
+}
+console.log(scuberGreetingForFeet(2600))
+
+function ternaryCheckCity(city){
+  let response = city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return response
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+console.log(ternaryCheckCity('Seattle'))
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message
+  switch(tip){
+    case ('generous'):
+      message = 'Thank you so much.'
+      break
+    case ('not as generous'):
+      message = 'Thank you.'
+      break
+    default:
+      message = 'Bye.'
+      break
+  }
+  return message
 }
+ //   if (tip >= 50){
+  //    message = 'Thank you so much.'
+  //   } else if (tip < 50 && tip >= 1) {
+  //     message = 'Thank you.'
+  //    }
+  //   else if (tip < 1) {
+  //    message = 'Bye.'
+  //   }
+  //   return message
+  //  }
+
+console.log(switchOnCharmFromTip('not as generous'))
